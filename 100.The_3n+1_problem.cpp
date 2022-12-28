a24766f996b1bde1c6ed14aea18253a46705a235
Berlin <evan1234.ek@gmail.com>
2022-12-28 11:31:42 +0600

    Solved

--- 100.The_3n+1_problem.cpp
+++ 100.The_3n+1_problem.cpp
@@ -12,7 +12,44 @@ int main()
 
     // Code Start From Here
 
-    
+    int a, b;
+
+    while ((cin >> a >> b))
+    {
+        int a1 = a;
+        int b1 = b;
+
+        if (a > b)
+            swap(a, b);
+
+        int mcl = 0, cl;
+
+        while (a <= b)
+        {
+            cl = 1;
+            unsigned int n = a;
+            while (n != 1)
+            {
+                if (n % 2 == 1)
+                {
+                    n = 3 * n + 1;
+                }
+                else
+                {
+                    n = n / 2;
+                }
+                cl++;
+            }
+
+            if (cl > mcl)
+            {
+                mcl = cl;
+            }
+            a++;
+        }
+
+        cout << a1 << " " << b1 << " " << mcl << endl;
+    }
 
     return 0;
 }
